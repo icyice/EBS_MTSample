0eb0eff7866e01d60f85ac6b235bc677ebdd8881
unknown <bleng@DEV-BLENG.gfhome.gamefly.com>
2013-01-22 23:01:53 -0800

    initial checking

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using MassTransit;
+using MassTransit.Log4NetIntegration;
+using Messages;
+using log4net.Config;
+
+namespace ConsoleSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        
+            Console.WriteLine("set up a subscriber");
+
+            XmlConfigurator.Configure();
+
+            Bus.Initialize(config =>
+            {
+                config.UseMsmq();
+                config.VerifyMsmqConfiguration();
+                config.UseMulticastSubscriptionClient();
+                config.UseControlBus();
+               
+                config.ReceiveFrom("msmq://localhost/test_end");
+                //config.Subscribe(x=>x.Consumer<Consumer>());
+                config.Subscribe(s => s.Instance(new Consumer()).Permanent());
+                config.UseLog4Net();
+                //config.Subscribe(subs =>
+                //{
+                //    subs.Handler<ProductChangeMessage>(msg => Console.WriteLine("fff"));
+                //    //subs.Handler<string>(msg => Console.WriteLine(msg));
+                //    //Console.Read();
+                //});
+            });
+
+            //var bus = ServiceBusFactory.New(sbc =>
+            //                                    {
+            //                                        sbc.UseMsmq();
+            //                                        sbc.UseMulticastSubscriptionClient();
+            //                                        sbc.ReceiveFrom("msmq://localhost/test_end");
+            //                                        sbc.UseLog4Net();
+            //                                        sbc.Subscribe(x=>x.Handler<ProductChangeMessage>(msg=>Console.WriteLine(msg)));
+            //                                    });
+           
+            Bus.Instance.WriteIntrospectionToConsole();
+            //Bus.Instance.WriteIntrospectionToConsole();
+           
+            Console.Read();
+        }
+
+     
+    }
+
+    public class Consumer : Consumes<ProductChangeMessage>.All
+    {
+        public void Consume(ProductChangeMessage message)
+        {
+            Console.WriteLine("start consuming message");
+
+            if (string.IsNullOrEmpty(message.Ids))
+            {
+                Console.WriteLine("update all products");
+
+            }
+            else
+            {
+                Console.WriteLine("update products for " + message.Ids);
+            }
+        }
+    }
+}
---
+++ ConsolePublisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MassTransit;
+using MassTransit.Log4NetIntegration;
+using Messages;
+using log4net.Config;
+
+namespace ConsolePublisher
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("start publishing");
+            XmlConfigurator.Configure();
+            //initialize bus 
+            Bus.Initialize(config =>
+            {
+                config.UseMsmq();
+                config.VerifyMsmqConfiguration();
+                config.UseMulticastSubscriptionClient();
+                config.UseControlBus();
+             
+                config.ReceiveFrom("msmq://localhost/test_queue");
+                config.UseLog4Net();
+
+            });
+            //Bus.Instance.WriteIntrospectionToConsole();
+           
+            //publish updating all products message
+            var msg1 = new ProductChangeMessage();
+
+            Bus.Instance.Publish(new ProductChangeMessage());
+            Console.WriteLine("Message sent 1");
+
+            //publish updating some products message
+            var msg2 = new ProductChangeMessage() { Ids ="1,2,3,4" };
+            Bus.Instance.Publish(msg2);
+            Console.WriteLine("Message sent 2");
+
+
+           
+            Console.Read();
+
+           
+        }
+    }
+}
